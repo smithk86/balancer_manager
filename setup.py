db0860e22602f0af1312b98394ea075476e28e26
Kyle Smith <ksmith@smc3.com>
2016-03-17 11:16:32 -0400

    1.4.0-dev

--- setup.py
+++ setup.py
@@ -4,11 +4,14 @@ from setuptools import setup
 
 setup(
     name='py_balancer_manager',
-    version='1.3.1-dev',
+    version='1.4.0-dev',
     description="Library for programatically interacting with Apache's mod_proxy_balancer management interface",
     author='Kyle Smith',
     author_email='smithk86@gmail.com',
     url='https://github.com/smithk86/balancer_manager',
+    packages=[
+        'py_balancer_manager'
+    ],
     install_requires=[
         'requests',
         'beautifulsoup4'
