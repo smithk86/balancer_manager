5be750145518fd788858f5579bbdab23f443fd28
Kyle Smith <ksmith@smc3.com>
2016-04-14 14:16:48 -0400

    fix packages to exclude tests

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='py_balancer_manager',
-    version='1.6.1-dev',
+    version='1.6.2-dev',
     description="Library for programatically interacting with Apache's mod_proxy_balancer management interface",
     author='Kyle Smith',
     author_email='smithk86@gmail.com',
     url='https://github.com/smithk86/balancer_manager',
-    packages=[
-        'py_balancer_manager'
-    ],
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     install_requires=[
         'requests',
         'beautifulsoup4'
