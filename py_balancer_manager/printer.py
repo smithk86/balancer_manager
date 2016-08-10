08cba57ab992a27182379ae8f9f9882caf840252
Kyle Smith <ksmith@smc3.com>
2016-08-10 10:36:43 -0400

    bugfix; rearrange verbose print for manage.py; version 1.9.2

--- py_balancer_manager/printer.py
+++ py_balancer_manager/printer.py
@@ -24,25 +24,24 @@ def routes(routes, verbose=False):
 
         rows.append([
             PrettyString('URL', 'bold'),
-            PrettyString('Apache Version', 'bold'),
             PrettyString('Cluster', 'bold'),
             PrettyString('Worker URL', 'bold'),
             PrettyString('Route', 'bold'),
-            PrettyString('Route Redir', 'bold'),
-            PrettyString('Factor', 'bold'),
-            PrettyString('Set', 'bold'),
+            PrettyString('Elected', 'bold'),
+            PrettyString('To', 'bold'),
+            PrettyString('From', 'bold'),
             PrettyString('Status: Ok', 'bold'),
+            PrettyString('Active', 'bold'),
             PrettyString('Status: Err', 'bold'),
             PrettyString('Status: Ign', 'bold'),
             PrettyString('Status: Drn', 'bold'),
             PrettyString('Status: Dis', 'bold'),
             PrettyString('Status: Stby', 'bold'),
-            PrettyString('Elected', 'bold'),
+            PrettyString('Route Redir', 'bold'),
+            PrettyString('Factor', 'bold'),
+            PrettyString('Set', 'bold'),
             PrettyString('Busy', 'bold'),
-            PrettyString('Load', 'bold'),
-            PrettyString('To', 'bold'),
-            PrettyString('From', 'bold'),
-            PrettyString('Session Nonce UUID', 'bold'),
+            PrettyString('Load', 'bold')
         ])
 
     else:
@@ -53,6 +52,7 @@ def routes(routes, verbose=False):
             PrettyString('Worker URL', 'bold'),
             PrettyString('Route', 'bold'),
             PrettyString('Status: Ok', 'bold'),
+            PrettyString('Active', 'bold'),
             PrettyString('Status: Err', 'bold'),
             PrettyString('Status: Ign', 'bold'),
             PrettyString('Status: Drn', 'bold'),
@@ -66,25 +66,24 @@ def routes(routes, verbose=False):
 
             rows.append([
                 PrettyString(_get_value(route['url']), 'cyan'),
-                _get_value(route['apache_version']),
                 _get_value(route['cluster']),
                 PrettyString(_get_value(route['worker']), 'yellow'),
                 _get_value(route['route']),
-                _get_value(route['route_redir']),
-                _get_value(route['factor']),
-                _get_value(route['set']),
+                _get_value(route['elected']),
+                _get_value(route['to']),
+                _get_value(route['from']),
                 _get_value(route['status_ok']),
+                _get_value(route['taking_traffic']),
                 _get_value(route['status_error']),
                 _get_value(route['status_ignore_errors']),
                 _get_value(route['status_draining_mode']),
                 _get_value(route['status_disabled']),
                 _get_value(route['status_hot_standby']),
-                _get_value(route['elected']),
+                _get_value(route['route_redir']),
+                _get_value(route['factor']),
+                _get_value(route['set']),
                 _get_value(route['busy']),
-                _get_value(route['load']),
-                _get_value(route['to']),
-                _get_value(route['from']),
-                _get_value(route['session_nonce_uuid'])
+                _get_value(route['load'])
             ])
 
     else:
@@ -96,6 +95,7 @@ def routes(routes, verbose=False):
                 PrettyString(_get_value(route['worker']), 'yellow'),
                 _get_value(route['route']),
                 _get_value(route['status_ok']),
+                _get_value(route['taking_traffic']),
                 _get_value(route['status_error']),
                 _get_value(route['status_ignore_errors']),
                 _get_value(route['status_draining_mode']),
