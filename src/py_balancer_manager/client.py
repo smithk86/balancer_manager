b427ac121f33ce128dcdfd6483bd35b89f887bd2
Kyle Smith <ksmith@smc3.com>
2017-04-04 10:02:27 -0400

    allow datetime object to be modifed in parent class

--- src/py_balancer_manager/client.py
+++ src/py_balancer_manager/client.py
@@ -289,7 +289,7 @@ class Client:
             raise BalancerManagerError(e)
 
         # update timestamp
-        self.updated_datetime = datetime.now()
+        self.updated_datetime = self._datetime_now()
         # process text with beautiful soup
         bsoup = BeautifulSoup(response.text, 'lxml')
         # update routes
@@ -314,7 +314,7 @@ class Client:
 
         # if there are no clusters or refresh=True or cluster ttl is reached
         if self.updated_datetime is None or self.clusters is None or refresh is True or \
-                (self.updated_datetime < (datetime.now() - timedelta(seconds=self.clusters_ttl))):
+                (self.updated_datetime < (self._datetime_now() - timedelta(seconds=self.clusters_ttl))):
             self.update()
 
         return self.clusters
@@ -443,7 +443,7 @@ class Client:
                     cluster.failover_attempts = int(cells[2].text)
                     cluster.method = cells[3].text
 
-            cluster.updated_datetime = datetime.now()
+            cluster.updated_datetime = self._datetime_now()
 
         # only iterate through even tables which contain route data
         for table in page_route_tables:
@@ -529,7 +529,7 @@ class Client:
                 else:
                     raise ValueError('this module only supports apache 2.2 and 2.4')
 
-                route.updated_datetime = datetime.now()
+                route.updated_datetime = self._datetime_now()
 
         # iterate clusters for post-parse processing
         for cluster in self.clusters:
@@ -602,3 +602,9 @@ class Client:
             logging.exception(e)
 
         return None
+
+    def _datetime_now(self):
+
+        """ modify datetime in parent class [IE: make timezone aware] """
+
+        return datetime.now()
