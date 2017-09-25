4f81a24412bf0a667352a1e2121fce9eff31a03d
Kyle Smith <ksmith@smc3.com>
2017-09-25 15:28:00 -0400

    remove redundant code

--- src/py_balancer_manager/__init__.py
+++ src/py_balancer_manager/__init__.py
@@ -1,7 +1,6 @@
 from .client import Client, Route, Cluster, BalancerManagerParseError
 from .validate import ValidationClient, ValidatedRoute, ValidatedCluster
 from .workflow import Workflow
-from .errors import BalancerManagerError, ValidationClientError
 from .printer import get_formated_routes, print_routes, get_formated_validated_routes, print_validated_routes
 from .prettystring import PrettyString
 from .errors import BalancerManagerError, MultipleBalancerManagerErrors, ValidationClientError, NotFound
