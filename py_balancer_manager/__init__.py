6284db3c02ead1e4e8760d58a8831a694afde867
Kyle Smith <ksmith@smc3.com>
2019-05-14 13:55:52 -0400

    packaging changes

--- py_balancer_manager/__init__.py
+++ py_balancer_manager/__init__.py
@@ -1,3 +1,14 @@
+import sys
+
+
+__VERSION__ = '3.0.0-dev'
+__MIN_PYTHON__ = (3, 7)
+
+
+if sys.version_info < __MIN_PYTHON__:
+    sys.exit('python {}.{} or later is required'.format(*__MIN_PYTHON__))
+
+
 from .client import Client
 from .cluster import Cluster
 from .errors import BalancerManagerError, TaskExceptions
--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python
 
+import os.path
+
 from setuptools import setup, find_packages
+
+
+# get the version to include in setup()
+dir_ = os.path.abspath(os.path.dirname(__file__))
+with open(f'{dir_}/py_balancer_manager/__init__.py') as fh:
+    for line in fh:
+        if '__VERSION__' in line:
+            exec(line)
+
 
 setup(
     name='py_balancer_manager',
-    version='3.0.0-dev',
+    version=__VERSION__,
     description="Library for programatically interacting with Apache's mod_proxy_balancer management interface",
     author='Kyle Smith',
     author_email='smithk86@gmail.com',
-    url='https://github.com/smithk86/balancer_manager',
+    url='https://github.com/smithk86/py-balancer-manager',
     entry_points={
         'console_scripts': [
             'balancer-manager-manage=py_balancer_manager.cli:manage',
@@ -20,19 +31,19 @@ setup(
         'py_balancer_manager.cli',
     ],
     install_requires=[
-        'aiohttp==3.5.4',
-        'beautifulsoup4==4.7.1',
-        'pytz==2019.1',
-        'tzlocal==1.5.1',
-        'python-dateutil==2.8.0',
-        'packaging==19.0'
+        'aiohttp',
+        'beautifulsoup4',
+        'packaging',
+        'pytz',
+        'python-dateutil',
+        'tzlocal'
     ],
     setup_requires=[
         'pytest-runner'
     ],
     tests_require=[
+        'docker',
         'pytest',
-        'pytest-asyncio',
-        'docker'
+        'pytest-asyncio'
     ]
 )
